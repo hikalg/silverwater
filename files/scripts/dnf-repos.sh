52d9a53403fb78f564cee6f1dc6f34bcab1cf2c1
Everest Luong <101264127+hikalg@users.noreply.github.com>
2025-09-19 21:26:34 +1000

    Revert "replace librewolf flatpak with dnf"
    This reverts commit 0d0686ce60218ebb2bd424fa3b37e0e95f57053f.

--- files/scripts/dnf-repos.sh
+++
@@ -1,9 +0,0 @@
-set -ouex pipefail
-
-# Terra
-
-curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | pkexec tee /etc/yum.repos.d/terra.repo
-sudo rpm-ostree install terra-release
-
-# Librewolf
-curl -fsSL https://repo.librewolf.net/librewolf.repo | pkexec tee /etc/yum.repos.d/librewolf.repo
